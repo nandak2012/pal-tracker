a5cc7c87847734204ca0fa670793799f6a7934e8
Nanda <nandk2012@gmail.com>
2017-11-14 11:06:06 -0600

    concourse changes

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,5 +1,6 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
+
+    private String hello_message;
+
+    public WelcomeController(@Value("${WELCOME_MESSAGE}") String hello_message) {
+        this.hello_message = hello_message;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return hello_message;
     }
+
 
 }
